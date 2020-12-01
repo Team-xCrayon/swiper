3137c464646d894d4d6c9fa9f5f777763d44cd89
xCrayon <944292511@qq.com>
2020-12-01 20:27:03 +0800

    '完成了用户模型'

--- swiper/settings.py
+++ swiper/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_r)29b0f4x5)$&hlt+3m%v%-0__-r5w=bd4&ecq#7(n465g5*-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
